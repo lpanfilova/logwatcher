d9dab50ef07f3b049582fa508eccb6e4078a65fd
Navpreet <gillnavpreetkaur8@gmail.com>
2026-02-21 14:22:11 -0500

    fix port type

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://backend-api:8080",
+        target: "http://backend-api:8000",
         changeOrigin: true,
       },
     },
